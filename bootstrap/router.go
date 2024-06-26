b0e1a2d5cd529d0dde73472051120abb6f61b6f7
liuandong <15056997431@189.cn>
2024-06-26 09:11:37 +0800

    feat: 跨域 保存错误日志

--- bootstrap/router.go
+++ bootstrap/router.go
@@ -1,13 +1,24 @@
 package bootstrap
 
 import (
+	"gin-web/app/middleware"
 	"gin-web/global"
 	"gin-web/routes"
 	"github.com/gin-gonic/gin"
 )
 
 func setupRouter() *gin.Engine {
-	router := gin.Default()
+	if global.App.Config.App.Env == "production" {
+		gin.SetMode(gin.ReleaseMode)
+	}
+	// 默认
+	//router := gin.Default()
+	router := gin.New()
+	router.Use(gin.Logger(), middleware.CustomRecovery())
+
+	// 跨域处理
+	router.Use(middleware.Cors())
+
 	router.StaticFile("/", "./static/index.html")
 
 	group := router.Group("/api")
